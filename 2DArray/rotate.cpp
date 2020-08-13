3fb0e9c27841f47e9ea8a6618594b51cacce697b
rockbuzz <sharma.aman7698@gmail.com>
2020-08-13 13:56:05 +0530

    rotate array inplace with and without rotate STL

--- 2DArray/rotate.cpp
+++ 2DArray/rotate.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
-bool magical_park(int a[][100],int m, int n){
+# include<algorithm>
+bool rotateExtraspace1(int a[][100],int m, int n){
 	int a1[100][100];
 	for(int i=0;i<m;i++){
 		for(int j=0;j<n; j++){
 			a1[n-j-1][i]=a[i][j];
-			cout<< "the i and the j value and the value assosiated is"<<n-j-1<<i<<a[i][j]<<endl;
+			//cout<< "the i and the j value and the value assosiated is"<<n-j-1<<i<<a[i][j]<<endl;
 		}
 	}
 	cout<<"the rotated matrix"<<endl;
@@ -16,6 +17,31 @@ bool magical_park(int a[][100],int m, int n){
 		cout<<endl;
 	}
 }
+
+
+void inplaceRotateSTL(int a[][100], int n){
+	//reverse each row 
+	for(int i=0; i<n;i++){
+		reverse(a[i],a[i]+n);
+	}
+	// transpose the array
+	for(int i=0;i<n; i++){
+		for(int j=0;j<n;j++){
+			if(i<j){
+				swap(a[i][j],a[j][i]);	
+			}
+			
+		}
+	}
+	cout<<"the rotated matrix"<<endl;
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			cout<<a[i][j]<<",";
+		}
+		cout<<endl;
+	}
+}
+
 int main(){
 	int m,n,k,s;
 	cout<<"enter two number for rows and columns respectively"<<endl;
@@ -36,7 +62,9 @@ int main(){
 		}
 		cout<<endl;
 	}
-	magical_park(a,m,n);
-
+	//rotateExtraspace1(a,m,n);
+	//inplacerotate() reverse of each row and transpose the complete array 	
+	//inplaceRotate(a,m);
+	inplaceRotateSTL(a,m);
 	return 0;
 }
